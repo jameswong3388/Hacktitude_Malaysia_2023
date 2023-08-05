f03901b833c055c245a062fe0174a1b2eb82c9ce
James wong <jameswong0517@gmail.com>
2023-08-05 15:12:51 +0800

    modified .gitignore jest.config.js file

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ export default async () => {
     Replace the <INSERT YOUR FILE NAME HERE> with the name of the cloned repo folder name 
     Eg: "../23da1134-b7a2-4e2f-bcdf-59dbab5a7595"
   */
-    let __dirname = "../<INSERT YOUR FILE NAME HERE>";
+    let __dirname = "../499f8c79-fa56-4273-b627-cb06c758ca0d";
 
   return {
     verbose: true,
