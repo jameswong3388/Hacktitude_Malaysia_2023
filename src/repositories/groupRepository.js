521c2f0a7d02bd9abcaf92dbb786143de1350958
AaronTan <tanweihup@gmail.com>
2023-08-05 22:51:56 +0800

    completed challenge 10

--- src/repositories/groupRepository.js
+++ src/repositories/groupRepository.js
@@ -1,4 +1,3 @@
-
 import Group from "../models/group.js";
 import HttpStatus from "../enums/httpStatus.js";
 import knex_db from "../../db/db-config.js";
@@ -6,251 +5,252 @@ import knex from "knex";
 
 let _db;
 function init(db) {
-  _db = db;
+    _db = db;
 }
 
 // Implement the method body for challenge 9
 async function getGroupsOfUser(userid) {
-  return new Promise((resolve, reject) => {
-    knex_db
-      .raw(
-        `
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(
+                `
         SELECT groups.*
         FROM groups
         JOIN userGroups ON groups.id = userGroups.group_id
         JOIN users ON userGroups.user_id = users.id
         WHERE users.id = ?
         `,
-        [userid]
-      )
-      .then((result) => {
-        const groups = result;
-        resolve(groups);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+                [userid]
+            )
+            .then((result) => {
+                const groups = result;
+                resolve(groups);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 async function getProjectsOfGroup(groupId) {
-  return new Promise((resolve, reject) => {
-    knex_db
-      .raw(
-        `SELECT pt.*, ut.image_url 
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(
+                `SELECT pt.*, ut.image_url 
       FROM projects pt JOIN users ut on ut.id = pt.ownerId
       WHERE groupId = ?`,
-        [groupId]
-      )
-      .then((result) => {
-        const projects = result;
-        resolve(projects);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+                [groupId]
+            )
+            .then((result) => {
+                const projects = result;
+                resolve(projects);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 async function getTasksOfUser(userId, groupId) {
-  return new Promise((resolve, reject) => {
-    knex_db
-      .raw(
-        ` SELECT tt.*, pt.name AS projectName, pt.projectStatus
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(
+                ` SELECT tt.*, pt.name AS projectName, pt.projectStatus
          FROM tasks tt LEFT JOIN projects pt ON tt.projectId = pt.id WHERE tt.assigneeId = ? AND pt.groupId = ?`,
-        [userId, groupId]
-      )
-      .then((result) => {
-        const tasks = result;
-        resolve(tasks);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+                [userId, groupId]
+            )
+            .then((result) => {
+                const tasks = result;
+                resolve(tasks);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 async function getTasksOfProject(projectId) {
-  return new Promise((resolve, reject) => {
-    knex_db
-      .raw(` SELECT tt.* FROM tasks tt WHERE tt.projectId = ?`, [projectId])
-      .then((result) => {
-        const tasks = result;
-        resolve(tasks);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(` SELECT tt.* FROM tasks tt WHERE tt.projectId = ?`, [
+                projectId,
+            ])
+            .then((result) => {
+                const tasks = result;
+                resolve(tasks);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 async function getUsersOfGroups(groupId) {
-  return new Promise((resolve, reject) => {
-    knex_db
-      .raw(
-        ` SELECT 
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(
+                ` SELECT 
         ut.id,
         ut.email,
+        ut.firstname,
+        ut.lastname,
         ut.image_url 
         FROM users ut
         LEFT JOIN userGroups ug
         ON ug.user_id=ut.id
         WHERE ug.group_id = ?`,
-        [groupId]
-      )
-      .then((result) => {
-        const users = result;
-        resolve(users);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+                [groupId]
+            )
+            .then((result) => {
+                const users = result;
+                resolve(users);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 // Implement this method body for challenge 11
-async function addNewProject(projectDetails) {
-
-}
+async function addNewProject(projectDetails) {}
 
 // Implement this method body for challenge 12
-async function addNewTask(taskDetails) {
-
-}
+async function addNewTask(taskDetails) {}
 
 // Implement this method for challenge 13
-async function updateProject(details, projectId) {
-
-}
+async function updateProject(details, projectId) {}
 
 // Implement this method for challenge 14
-async function updateTask(details, taskId) {
-
-}
+async function updateTask(details, taskId) {}
 
 // Implement this method for challenge 15
-async function updateProjectStatus(projectId, status) {
-
-}
+async function updateProjectStatus(projectId, status) {}
 
 // Implement this method for challenge 16
-async function updateTaskStatus(taskId, status) {
-
-}
+async function updateTaskStatus(taskId, status) {}
 
 async function getProjectById(projectId) {
-  return new Promise((resolve, reject) => {
-    knex_db
-      .raw(` SELECT * FROM projects WHERE id = ?`, [projectId])
-      .then((result) => {
-        const project = result;
-        resolve(project);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(` SELECT * FROM projects WHERE id = ?`, [projectId])
+            .then((result) => {
+                const project = result;
+                resolve(project);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
-
 
 // Implement this method for Challenge 5
 async function getGroupsFromKeyword(keyword) {
-  return new Promise((resolve, reject) => {
-    knex_db
-      .select('*')
-      .from('projects')
-      .where('name', 'LIKE', `%${keyword}%`)
-      .then((result) => {
-        const project = result;
-        resolve(project);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        knex_db
+            .select("*")
+            .from("projects")
+            .where("name", "LIKE", `%${keyword}%`)
+            .then((result) => {
+                const project = result;
+                resolve(project);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 // Implement this method for Challenge 5
 async function addNewGroup(data) {
-  return new Promise((resolve, reject) => {
-    knex_db('groups')
-      .insert({
-        name: data.group_name,
-        description: data.group_desc,
-        hobbies: `[${data.group_name}]`,
-        capacity: Math.floor(Math.random() * 101),
-      })
-      .returning('id')
-      .then((result) => {
-        resolve({ message: 'New group added successfully.', id: result[0]});
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        knex_db("groups")
+            .insert({
+                name: data.group_name,
+                description: data.group_desc,
+                hobbies: `[${data.group_name}]`,
+                capacity: Math.floor(Math.random() * 101),
+            })
+            .returning("id")
+            .then((result) => {
+                resolve({
+                    message: "New group added successfully.",
+                    id: result[0],
+                });
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 // Implement this method for Challenge 6
 async function addUserToGroup(data) {
-  return new Promise((resolve, reject) => {
-    knex_db('userGroups')
-      .insert({
-        group_id: data.group_id,
-        user_id: data.user_id,
-      })
-      .then((result) => {
-        resolve({ message: 'User add to group successfully.'});
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        knex_db("userGroups")
+            .insert({
+                group_id: data.group_id,
+                user_id: data.user_id,
+            })
+            .then((result) => {
+                resolve({ message: "User add to group successfully." });
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 // Implement this method for challenge 6
 async function getGroupsFromUser(userId) {
-  return new Promise((resolve, reject) => {
-    knex_db('userGroups')
-      .where('user_id', userId)
-      .then((userGroups) => {
-        // Extract groupIds from userGroups
-        const groupIds = userGroups.map(userGroup => userGroup.group_id);
+    return new Promise((resolve, reject) => {
+        knex_db("userGroups")
+            .where("user_id", userId)
+            .then((userGroups) => {
+                // Extract groupIds from userGroups
+                const groupIds = userGroups.map(
+                    (userGroup) => userGroup.group_id
+                );
 
-        // Fetch groups with matching groupIds
-        return knex_db('groups')
-          .whereIn('id', groupIds);
-      })
-      .then((groups) => {
-        resolve(groups);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+                // Fetch groups with matching groupIds
+                return knex_db("groups").whereIn("id", groupIds);
+            })
+            .then((groups) => {
+                resolve(groups);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 function parseGroupsData(data) {
-  return data.map(item => {
-    return new Group(item.id, item.name, item.description, JSON.parse(item.hobbies), item.capacity)
-  });
+    return data.map((item) => {
+        return new Group(
+            item.id,
+            item.name,
+            item.description,
+            JSON.parse(item.hobbies),
+            item.capacity
+        );
+    });
 }
 
 export default {
-  init,
-  getGroupsOfUser,
-  getProjectsOfGroup,
-  getTasksOfUser,
-  getTasksOfProject,
-  getUsersOfGroups,
-  updateProject,
-  updateTask,
-  getProjectById,
-  updateProjectStatus,
-  updateTaskStatus,
-  addNewProject,
-  addNewTask,
-  getGroupsFromUser,
-  addUserToGroup,
-  getGroupsFromKeyword,
-  addNewGroup
+    init,
+    getGroupsOfUser,
+    getProjectsOfGroup,
+    getTasksOfUser,
+    getTasksOfProject,
+    getUsersOfGroups,
+    updateProject,
+    updateTask,
+    getProjectById,
+    updateProjectStatus,
+    updateTaskStatus,
+    addNewProject,
+    addNewTask,
+    getGroupsFromUser,
+    addUserToGroup,
+    getGroupsFromKeyword,
+    addNewGroup,
 };
--- src/services/groupService.js
+++ src/services/groupService.js
@@ -4,40 +4,40 @@ import dbConnection from "../../sqlite.js";
 
 //initialize db connection
 function initializeApp() {
-  dbConnection
-    .getDbConnection()
-    .then((db) => {
-      groupRepository.init(db);
-    })
-    .catch((err) => {
-      console.error("Error initializing the application:", err);
-      process.exit(1); // Exit the application or handle the error appropriately
-    });
+    dbConnection
+        .getDbConnection()
+        .then((db) => {
+            groupRepository.init(db);
+        })
+        .catch((err) => {
+            console.error("Error initializing the application:", err);
+            process.exit(1); // Exit the application or handle the error appropriately
+        });
 }
 
 async function getGroupsOfUserReq(id) {
-  const response = await groupRepository.getGroupsOfUser(id);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.getGroupsOfUser(id);
+    return { response: response, status: httpStatus.OK };
 }
 
 async function getProjectsOfGroupReq(id) {
-  const response = await groupRepository.getProjectsOfGroup(id);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.getProjectsOfGroup(id);
+    return { response: response, status: httpStatus.OK };
 }
 
 async function getTasksOfUserReq(id, groupId) {
-  const response = await groupRepository.getTasksOfUser(id, groupId);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.getTasksOfUser(id, groupId);
+    return { response: response, status: httpStatus.OK };
 }
 
 async function getTasksOfProjectReq(projectId) {
-  const response = await groupRepository.getTasksOfProject(projectId);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.getTasksOfProject(projectId);
+    return { response: response, status: httpStatus.OK };
 }
 
 async function getUsersOfGroupsReq(groupId) {
-  const response = await groupRepository.getUsersOfGroups(groupId);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.getUsersOfGroups(groupId);
+    return { response: response, status: httpStatus.OK };
 }
 
 // Implement the method updateProjectReq(details, projectId) for challenge 13 here
@@ -47,7 +47,6 @@ async function getUsersOfGroupsReq(groupId) {
 //
 //
 
-
 // Implement the method updateTaskReq(details, taskId) for challenge 14 here
 //
 //
@@ -56,8 +55,8 @@ async function getUsersOfGroupsReq(groupId) {
 //
 
 async function getProjectByIdReq(projectId) {
-  const response = await groupRepository.getProjectById(projectId);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.getProjectById(projectId);
+    return { response: response, status: httpStatus.OK };
 }
 
 // Implement the method updateProjectStatusReq(projectId, status) for challenge 15 here
@@ -75,57 +74,57 @@ async function getProjectByIdReq(projectId) {
 //
 
 async function addNewProjectReq(projectDetails) {
-  const response = await groupRepository.addNewProject(projectDetails);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.addNewProject(projectDetails);
+    return { response: response, status: httpStatus.OK };
 }
 
 async function addNewTaskReq(taskDetails) {
-  const response = await groupRepository.addNewTask(taskDetails);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.addNewTask(taskDetails);
+    return { response: response, status: httpStatus.OK };
 }
 // Implement this method for Challenge 5
 async function getGroupsFromKeyword(keyword) {
-  const response = await groupRepository.getGroupsFromKeyword(keyword);
+    const response = await groupRepository.getGroupsFromKeyword(keyword);
 
-  if (response ) {
-    return { response: response, statusbar: httpStatus.OK }
-  } else {
-    return { response: [], statusbar: httpStatus.OK }
-  }
+    if (response) {
+        return { response: response, statusbar: httpStatus.OK };
+    } else {
+        return { response: [], statusbar: httpStatus.OK };
+    }
 }
 
 // Implement this method for Challenge 5
 async function addNewGroup(data) {
-  const response = await groupRepository.addNewGroup(data);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.addNewGroup(data);
+    return { response: response, status: httpStatus.OK };
 }
 
 // Implement this method for Challenge 6
 async function addUserToGroup(data) {
-  const response = await groupRepository.addUserToGroup(data);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.addUserToGroup(data);
+    return { response: response, status: httpStatus.OK };
 }
 
 // Implement this method for Challenge 6
 async function getGroupsFromUser(user_id) {
-  const response = await groupRepository.getGroupsFromUser(user_id);
-  return { response: response, status: httpStatus.OK };
+    const response = await groupRepository.getGroupsFromUser(user_id);
+    return { response: response, status: httpStatus.OK };
 }
 
 initializeApp();
 
 export default {
-  initializeApp,
-  getGroupsOfUserReq,
-  getProjectsOfGroupReq,
-  getTasksOfUserReq,
-  getTasksOfProjectReq,
-  getUsersOfGroupsReq,
-  getProjectByIdReq,
-  getGroupsFromKeyword,
-  getGroupsFromUser,
-  addNewProjectReq,
-  addNewTaskReq,
-  addNewGroup,
-  addUserToGroup,
+    initializeApp,
+    getGroupsOfUserReq,
+    getProjectsOfGroupReq,
+    getTasksOfUserReq,
+    getTasksOfProjectReq,
+    getUsersOfGroupsReq,
+    getProjectByIdReq,
+    getGroupsFromKeyword,
+    getGroupsFromUser,
+    addNewProjectReq,
+    addNewTaskReq,
+    addNewGroup,
+    addUserToGroup,
 };
